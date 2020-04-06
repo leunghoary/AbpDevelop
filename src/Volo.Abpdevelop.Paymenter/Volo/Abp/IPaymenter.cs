93bc97f4d6fb1317ad0f153c0e93bc1b06bfc31c
278966768@qq.com <278966768@qq.com>
2020-04-06 16:14:21 +0800

    初次提交

---
+++ src/Volo.Abpdevelop.Paymenter/Volo/Abp/IPaymenter.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Volo.Abp
+{
+    public interface IPaymenter
+    {
+        /// <summary>
+        /// 扫码支付
+        /// </summary>
+        /// <returns></returns>
+        Task MicroPayAsync();
+
+        /// <summary>
+        /// 统一下单
+        /// </summary>
+        /// <returns></returns>
+        Task UnifiedOrderAsync();
+
+    }
+}
